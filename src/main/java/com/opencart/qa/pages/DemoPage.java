28313ebc2b35b52020bfb592d6070fb83e14abc0
priyab1_lab <priyabhoyar260@gmail.com>
2026-09-11 08:09:45 +0530

    updated login page and added Demopage

---
+++ src/main/java/com/opencart/qa/pages/DemoPage.java
@@ -0,0 +1,16 @@
+package com.opencart.qa.pages;
+
+public class DemoPage {
+int i=1;
+int j=2;
+int sum;
+	public static void main(String[] args) {
+		 DemoPage obj = new DemoPage();
+		 obj.sum = obj.i + obj.j;
+
+	        System.out.println(obj.sum);
+		
+
+	}
+
+}
