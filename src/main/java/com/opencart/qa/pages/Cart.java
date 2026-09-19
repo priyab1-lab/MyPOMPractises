ca6128909ef36c2e57510f8605f2f29523136299
priyab1_lab <priyabhoyar260@gmail.com>
2026-09-19 18:42:51 +0530

    updated the PR

--- src/main/java/com/opencart/qa/pages/Cart.java
+++ src/main/java/com/opencart/qa/pages/Cart.java
@@ -5,14 +5,21 @@ public class Cart {
 	int i=1;
 	int j=2;
 	int sum;
+	int mul;
 	
 	public void sum() {
 		sum=i+j;
 		System.out.println(sum);
 	}
+	public void mul() {
+		mul=i*j;
+		System.out.println(mul);
+	}
+	
 	public static void main(String[] args) {
 		Cart crt=new Cart();
 		crt.sum();
+		crt.mul();
 	}
 
 }
