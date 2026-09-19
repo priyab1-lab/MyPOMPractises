5ac8a46e68fb36e8dd4a94da2bffdc67d546fd90
priyab1_lab <priyabhoyar260@gmail.com>
2026-09-19 18:28:50 +0530

    created a separate branch

---
+++ src/main/java/com/opencart/qa/pages/Cart.java
@@ -0,0 +1,18 @@
+package com.opencart.qa.pages;
+
+public class Cart {
+
+	int i=1;
+	int j=2;
+	int sum;
+	
+	public void sum() {
+		sum=i+j;
+		System.out.println(sum);
+	}
+	public static void main(String[] args) {
+		Cart crt=new Cart();
+		crt.sum();
+	}
+
+}
